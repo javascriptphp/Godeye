ba1ac3171dd021fd0beb098760ca1d240882141e
SylvieHsu <sylviehsu@outlook.com>
2024-11-15 23:24:30 +0800

    feat: add markpoint when above or below threshold

--- components/charts/realtimeBuyConfig.ts
+++ components/charts/realtimeBuyConfig.ts
@@ -10,6 +10,36 @@ export const buildCustomConfig = function ({
     data: RealtimeData;
     t: TFunction;
 }) {
+    const highThreshold = data.threshold2;
+    const lowThreshold = data.threshold;
+    const markPoints = data.metric
+        .map((value, index) => {
+            if (
+                highThreshold &&
+                value > highThreshold &&
+                (index === 0 || data.metric[index - 1] < highThreshold)
+            ) {
+                return {
+                    coord: [index, data.high[index]],
+                    value: "SELL",
+                    name: "sell",
+                    itemStyle: { color: "red" },
+                };
+            } else if (
+                value < lowThreshold &&
+                (index === 0 || data.metric[index - 1] > lowThreshold)
+            ) {
+                return {
+                    coord: [index, data.low[index]],
+                    value: "BUY",
+                    name: "buy",
+                    itemStyle: { color: "green" },
+                };
+            }
+            return null;
+        })
+        .filter((point) => point !== null);
+
     return {
         tooltip: {
             trigger: "axis",
@@ -17,19 +47,24 @@ export const buildCustomConfig = function ({
                 type: "cross",
             },
         },
-        grid: [
-            {
-                left: 70,
-                right: 50,
-                height: "35%",
-            },
-            {
-                left: 70,
-                right: 50,
-                top: "55%",
-                height: "35%",
-            },
-        ],
+        grid: {
+            left: 70,
+            right: 50,
+            top: 90,
+        },
+        // grid: [
+        //     {
+        //         left: 70,
+        //         right: 50,
+        //         height: "35%",
+        //     },
+        //     {
+        //         left: 70,
+        //         right: 50,
+        //         top: "55%",
+        //         height: "35%",
+        //     },
+        // ],
         dataZoom: [
             {
                 show: true,
@@ -60,12 +95,12 @@ export const buildCustomConfig = function ({
                 splitLine: { show: false },
                 data: data.timestamps,
             },
-            {
-                gridIndex: 1,
-                type: "category",
-                splitLine: { show: false },
-                data: data.timestamps,
-            },
+            // {
+            //     gridIndex: 1,
+            //     type: "category",
+            //     splitLine: { show: false },
+            //     data: data.timestamps,
+            // },
         ],
         yAxis: [
             {
@@ -82,47 +117,76 @@ export const buildCustomConfig = function ({
                     formatter: (value: any) => value.toFixed(3),
                 },
             },
-            {
-                gridIndex: 1,
-                name: t("chart_metric"),
-                nameLocation: "end",
-                nameTextStyle: {
-                    fontSize: 14,
-                },
-                type: "value",
-                min: (value: any) => value.min * 0.999,
-                max: (value: any) => value.max * 1.001,
-                axisLabel: {
-                    formatter: (value: any) => value.toFixed(3),
-                },
-            },
+            // {
+            //     gridIndex: 1,
+            //     name: t("chart_metric"),
+            //     nameLocation: "end",
+            //     nameTextStyle: {
+            //         fontSize: 14,
+            //     },
+            //     type: "value",
+            //     min: (value: any) => value.min * 0.999,
+            //     max: (value: any) => value.max * 1.001,
+            //     axisLabel: {
+            //         formatter: (value: any) => value.toFixed(3),
+            //     },
+            // },
         ],
         series: [
             {
                 name: `${symbol} ${t("text_price")}`,
                 type: "candlestick",
                 data: transformToRawData(data),
-            },
-            {
-                name: `${t("text_indicator")}`,
-                type: "line",
-                showSymbol: false,
-                xAxisIndex: 1,
-                yAxisIndex: 1,
-                data: data.metric,
-                emphasis: {
-                    focus: "series",
-                },
-                itemStyle: {
-                    color: "#e98734",
+                markPoint: {
+                    data: markPoints,
                 },
             },
+            // {
+            //     name: `${t("text_indicator")}`,
+            //     type: "line",
+            //     showSymbol: false,
+            //     xAxisIndex: 1,
+            //     yAxisIndex: 1,
+            //     data: data.metric,
+            //     emphasis: {
+            //         focus: "series",
+            //     },
+            //     itemStyle: {
+            //         color: "#e98734",
+            //     },
+            //     markPoint: {
+            //         data: markPoints,
+            //     },
+            //     markLine: {
+            //         symbol: ["none", "none"], // No arrow symbols
+            //         data: [
+            //             data.threshold2
+            //                 ? {
+            //                       name: "High Threshold",
+            //                       yAxis: data.threshold2,
+            //                       lineStyle: {
+            //                           color: "red",
+            //                           width: 1.5,
+            //                       },
+            //                   }
+            //                 : undefined,
+            //             {
+            //                 name: "Low Threshold",
+            //                 yAxis: data.threshold,
+            //                 lineStyle: {
+            //                     color: "green",
+            //                     width: 1.5,
+            //                 },
+            //             },
+            //         ].filter(Boolean),
+            //     },
+            // },
             {
                 // 创建一个隐藏的 series 以模拟 markArea 的图例
                 name: t("text_buy_area"),
                 type: "line",
-                xAxisIndex: 1,
-                yAxisIndex: 1,
+                xAxisIndex: 0,
+                yAxisIndex: 0,
                 data: [],
                 itemStyle: { color: buyAreaColor, opacity: 0.9 },
             },
--- types/data.d.ts
+++ types/data.d.ts
@@ -2,6 +2,7 @@ interface RealtimeData {
     timestamps: string[];
     metric: number[];
     threshold: number;
+    threshold2?: number;
     open: number[];
     close: number[];
     high: number[];

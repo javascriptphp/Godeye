04dec951b831e64fee701d67e109508c77ab9af3
tianc43 <tianc43@qq.com>
2025-03-29 12:13:43 +0800

    prune: nft-mint

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,7 @@
-import Layout from "@/components/Layout";
-import HomePage from "@/pages/home";
+import KOLIPage from "@/pages/koli";
 
 export default function Home() {
-    return (
-        <Layout>
-            <HomePage />
-        </Layout>
-    );
+	return (
+		<KOLIPage/>
+	);
 }
